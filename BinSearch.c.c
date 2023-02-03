51de3aef0a93c28ea194dd542cda1d27ce96768c
Sripada  Praneetha <113002480+praneetha1286@users.noreply.github.com>
2023-02-03 22:32:45 +0530

    Add files via upload

---
+++ BinSearch.c.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int binary(int a[15], int low, int high, int key)
+{
+	int mid, i;	
+	while(low <= high)
+	{
+		mid = (high + low)/ 2;
+		if(key < a[mid])
+			high = mid - 1;
+		else if(key > a[mid])
+			low = mid + 1;
+		else
+			return mid;
+	}
+	return -1;
+}
+
+void main()
+{
+	int n, arr[15], i, idx, k;
+	printf("Enter size of array: ");
+	scanf("%d", &n);
+	printf("Enter %d sorted elements: ", n);
+	for(i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+	printf("Enter key to find: ");
+	scanf("%d", &k);
+	idx = binary(arr, 0, n - 1, k);
+	if(idx == -1)
+		printf("Element %d is not present\n", k);
+	else
+		printf("Element is present at %d position\n", idx + 1);
+}
